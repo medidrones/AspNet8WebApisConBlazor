362272a142493a26045b07b7b0765e9dbacee33a
Jorge Medina <medidrones@gmail.com>
2024-05-29 00:31:11 -0300

    Feat: Blazor - Implementando Web Api y Postgres data

---
+++ Firebase/src/blazor/Models/Producto.cs
@@ -0,0 +1,9 @@
+﻿namespace blazor.Models;
+
+public class Producto
+{
+    public int Id { get; set; }
+    public string? Nombre { get; set; }
+    public string? Descripcion { get; set; }
+    public decimal Precio { get; set; }
+}
